40fda56345c1c637caaa6e0dd86f979f7c5ab923
Shahzad <mech1090@gmail.com>
2022-06-25 10:30:58 -0600

    service created

---
+++ controller/user.controller.js
@@ -0,0 +1,18 @@
+
+
+
+const getLoginForm = (req,res)=>{
+    return res.render('login/layout')
+}
+
+const login = async(req,res)=>{}
+
+const getSignupForm = (req,res)=>{
+    return res.render('signup/layout')
+}
+
+const signup = async(req,res)=>{
+
+}
+
+module.exports = {getLoginForm,login,getSignupForm,signup}
